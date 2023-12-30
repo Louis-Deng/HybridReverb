ea5b775cfb0d6b7fe1e010b3f93628cd48904553
Louis-Deng <68354596+Louis-Deng@users.noreply.github.com>
2023-12-30 18:19:20 -0500

    Update 12.30

---
+++ Source/util.cpp
@@ -0,0 +1,26 @@
+//
+//  util.cpp
+//  RTConvolve
+//
+//  Created by Graham Barab on 2/5/17.
+//
+//
+
+#include "util.h"
+
+int isPowerOfTwo (unsigned int x)
+{
+    return ((x != 0) && !(x & (x - 1)));
+}
+
+
+void* checkNull(void *x)
+//void *checkNull(std::unique_ptr& x)
+{
+    if (x == nullptr)
+    {
+        std::cerr << "Error: could not allocate memory" << std::endl;
+        throw std::exception();
+    }
+    return x;
+}
